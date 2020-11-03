3a7bba27891a8049f9c37ef433b69396ef616822
Soma Gáspár <gaspar.soma@gmail.com>
2020-11-03 18:51:10 +0100

    -Hero Java created -Horvath Java Created

--- src/sample/Hero.java
+++ src/sample/Hero.java
@@ -2,5 +2,61 @@ package sample;
 
 
 public class Hero{
+    short xIndex;
+    short yIndex;
+    short Health;
+    short Attack;
+    short Movement;
+    short InitativeValue;
+
+
+    public short getxIndex() {
+        return xIndex;
+    }
+
+    public void setxIndex(short xIndex) {
+        this.xIndex = xIndex;
+    }
+
+    public short getyIndex() {
+        return yIndex;
+    }
+
+    public void setyIndex(short yIndex) {
+        this.yIndex = yIndex;
+    }
+
+    public short getHealth() {
+        return Health;
+    }
+
+    public void setHealth(short health) {
+        Health = health;
+    }
+
+    public short getAttack() {
+        return Attack;
+    }
+
+    public void setAttack(short attack) {
+        Attack = attack;
+    }
+
+    public short getMovement() {
+        return Movement;
+    }
+
+    public void setMovement(short movement) {
+        Movement = movement;
+    }
+
+    public short getInitativeValue() {
+        return InitativeValue;
+    }
+
+    public void setInitativeValue(short initativeValue) {
+        InitativeValue = initativeValue;
+    }
 
 }
+
---
+++ src/sample/Horvath.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Horvath extends Hero {
+
+    public void Altato(int xPos, int yPos)
+    {
+        //Az altato 3 egységnyi radiusban 1 körre elaltat mindent.
+        int CharacterPosition;
+
+
+    }
+
+}
