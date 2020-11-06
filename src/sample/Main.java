b4c7e52dab85aeb4646f06dfe5ec25bb14a0296c
kukodam <kukodam@gmail.com>
2020-11-06 08:56:38 +0100

    Fxml alap

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,30 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
 import javafx.stage.Stage;
 
+
+
+
 public class Main extends Application {
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
-    }
 
+
+
+    }
 
     public static void main(String[] args) {
         launch(args);
